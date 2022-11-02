e4da5c12724c12da2ce60e55580a7c4bfc8dacfe
Abui <adjiem31@gmail.com>
2022-11-02 19:53:10 +0700

    feat: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', 'jsx-a11y', 'prettier'],
+  rules: {},
+};
