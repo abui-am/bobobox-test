c242be706ec46868a038c86ebd44e372b59069ef
Abui <adjiem31@gmail.com>
2022-11-02 20:35:38 +0700

    feat: structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = (): JSX.Element => {
+  return <div className="text-2xl font-bold">Hello Mr. </div>;
+};
+
+export default App;
